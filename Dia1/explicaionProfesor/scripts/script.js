b8aa235bf7baa8a7e0f254c92ce0adad750f3699
William <williamcodcpro@gmail.com>
2025-07-18 11:59:31 -0500

    feat: :sparkles: Day 1 JavaScrip

---
+++ Dia1/explicaionProfesor/scripts/script.js
@@ -0,0 +1,34 @@
+// ###########################
+// ####### DIA 13 ############
+//############################
+
+// ¡¡¡Primer comando!!!!!!!!
+console.log("Hola mundo");
+
+//Number
+variable1 = 3.14;
+console.log(variable1);
+console.log(typeof variable1);
+
+//Concatenacion
+palabra1 = "Campus";
+palabra2 = "Lands";
+palabra3 = palabra1 + palabra2;
+console.log(palabra3);
+
+nombrePersona = "Luis Angel";
+plataActual = 55000;
+console.log("La persona llamda", nombrePersona, "tiene en su cuenta bancaria", plataActual, "pesos colombianos!");
+
+//Booleanos
+booleanito1 = false;
+booleanito2 = false;
+console.log(typeof booleanito1);
+
+//Condicionales
+if(booleanito1 == true){
+    console.log("Es verdadero !!!!!");
+}else {
+    console.log("No es verdadero :sadfeis:")
+}
+// Desarrollado por : William Santiago Pabon Ardila - C.C 1.006.798.412

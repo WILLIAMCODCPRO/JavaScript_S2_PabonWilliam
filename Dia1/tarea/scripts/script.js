a72f2afb3a7dc6413b3fbbd13f8e00bc5e5d2220
WILLIAMCODCPRO <williamcodcpro@gmail.com>
2025-07-19 12:58:40 -0500

    feat: :sparkles: add main menu

---
+++ Dia1/tarea/scripts/script.js
@@ -0,0 +1,20 @@
+let boolianito = true;
+
+while(boolianito = true) {
+ menu = prompt(`
+ =============================================
+          Simulador de Gasto Diario
+ =============================================
+ Seleccione una opción:
+
+ 1. Registrar nuevo gasto
+ 2. Listar gastos
+ 3. Calcular total de gastos
+ 4. Generar reporte de gastos
+ 5. Salir
+ =============================================
+`);
+ 
+ 
+ 
+}

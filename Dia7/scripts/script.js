1d8840b4f3b7cbb0bb6030db4510cc8c5cd66e90
WILLIAMCODCPRO <williamcodcpro@gmail.com>
2025-07-28 23:16:30 -0500

    feat: :sparkles: add api with XMLHttpRequest

--- Dia7/scripts/script.js
+++ Dia7/scripts/script.js
@@ -1,1 +1,24 @@
-let personaje = parent(prompt("Escribe el ID del personaje"));
+let nombrePersoanje = prompt("Escribe el nombre del personaje:");
+
+let llamadaApi = new XMLHttpRequest();
+
+llamadaApi.onload = function () {
+  
+    let respuestaApi = JSON.parse(llamadaApi.responseText);
+    let listaPersonajes = "";
+
+    for (let i = 0; i < respuestaApi["results"].length; i++) {
+      let personajeDeLaLista = respuestaApi["results"][i];
+      listaPersonajes += personajeDeLaLista["name"] + "\n"
+      alert(listaPersonajes)
+        
+    }
+
+  
+  
+};
+
+llamadaApi.open("GET", "https://rickandmortyapi.com/api/character/?name=" + (nombrePersoanje));
+llamadaApi.send();
+
+

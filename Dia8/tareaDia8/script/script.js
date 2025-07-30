f693d91330a76a997807f303954d7398cddc5fd6
WILLIAMCODCPRO <williamcodcpro@gmail.com>
2025-07-29 21:54:11 -0500

    feat: :sparkles: api homework

--- Dia8/tareaDia8/script/script.js
+++ Dia8/tareaDia8/script/script.js
@@ -1,25 +1,49 @@
 
+function buscarPorId (){
 
-let llamadaApi = new XMLHttpRequest();
+  let idPersonaje = document.getElementById("idInput").value;
+  let llamadaApi = new XMLHttpRequest();
 
-llamadaApi.open("GET", "https://swapi.py4e.com/api/people/1/" );
-
-llamadaApi.onload = function () {
   
+
+  llamadaApi.open("GET", `https://swapi.py4e.com/api/people/${idPersonaje}/` ); 
+
+  llamadaApi.onreadystatechange = function () {
+
+    
+
+    
     let respuestaApi = JSON.parse(llamadaApi.responseText);
-    let listaPersonajes = "";
+    document.getElementById("name").textContent = `Name: ${respuestaApi["name"]}`;
+    document.getElementById("height").textContent = `Height: ${respuestaApi["height"]}`;
+    document.getElementById("mass").textContent = `Mass: ${respuestaApi["mass"]}`;
+    document.getElementById("hairColor").textContent = `Hair Color: ${respuestaApi["hair_color"]}`;
+    document.getElementById("skinColor").textContent = `Skin Color: ${respuestaApi["skin_color"]}`;
+    document.getElementById("eyeColor").textContent = `Eye Color: ${respuestaApi["eye_color"]}`;
+    document.getElementById("birthYear").textContent = `Birth Year: ${respuestaApi["birth_year"]}`;
+    document.getElementById("gender").textContent = `Gender: ${respuestaApi["gender"]}`;
+    document.getElementById("homeworld").textContent = `Homeworld: ${respuestaApi["homeworld"]}`;
+    document.getElementById("films").textContent = `Films: ${respuestaApi["films"]}`;
+    document.getElementById("species").textContent = `Species: ${respuestaApi["species"]}`;
+    document.getElementById("vehicles").textContent = `Vehicles: ${respuestaApi["vehicles"]}`;
+    document.getElementById("starships").textContent = `Starships: ${respuestaApi["starships"]}`;
+    document.getElementById("created").textContent = `Created: ${respuestaApi["created"]}`;
+    document.getElementById("edited").textContent = `Edited: ${respuestaApi["edited"]}`;
+    document.getElementById("url").textContent = `URL: ${respuestaApi["url"]}`;
 
-    for (let i = 0; i < respuestaApi[i].length; i++) {
-      
-      listaPersonajes += personajeDeLaLista["name"] + "\n"
-        
-      
-        
-    }
 
-    alert(listaPersonajes)
   
-};
+    
+
+    
+
+  
+   }
+
+   llamadaApi.send();
+  
+}
 
 
-llamadaApi.send();
+
+

34094fa4da5ebec9c1f342046ae30f46a55adad6
WILLIAMCODCPRO <williamcodcpro@gmail.com>
2025-08-02 19:55:06 -0500

    feat: :sparkles: add API consumption

--- Dia11/scripts/scripts.js
+++ Dia11/scripts/scripts.js
@@ -1,3 +1,28 @@
-function jugar(){
-    
+let puntosJugador = "";
+let puntosMaquina = "";
+
+let llamarCartas = new XMLHttpRequest();
+
+llamarCartas.open("GET", "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
+
+llamarCartas.onreadystatechange = function () {
+
+    if(llamarCartas.readyState === 4 && llamarCartas.status === 200) {
+        try{
+
+            let barajaDeCartas = JSON.parse(llamarCartas.responseText)
+            console.log(barajaDeCartas)
+
+
+
+        }
+
+        catch(err){
+            
+            console.log(err.message);
+        }
+    }
+
 }
+
+llamarCartas.send()

7fb5a4b20c4aeae43d2656727eb1d6cda5685525
WILLIAMCODCPRO <williamcodcpro@gmail.com>
2025-08-05 17:25:59 -0500

    feat: :sparkles: homework 1

--- Dia13/scripts/script.js
+++ Dia13/scripts/script.js
@@ -1,25 +1,39 @@
 let token = "872fb6dc7f6e3e2a9ee0dcb4980ccd9b"
 
-
 function buscarHeroe(){
-    let nombreHeroe = document.getElementById("nombre")
-    nombreHeroe.addEventListener("keyup",function(presionarEnter){
-        if (presionarEnter.key === "Enter"){
-            let heroeParaBuscar = nombreHeroe.value
-            let llamadaHeroe = new XMLHttpRequest
-            llamadaHeroe.open("GET", `https://superheroapi.com/api/${token}/search/${heroeParaBuscar}`)
-            llamadaHeroe.onreadystatechange = function(){
-                if(llamadaHeroe.readyState === 4 && llamadaHeroe.status === 200){
-                    let datosHeroes = JSON.parse(llamadaHeroe.responseText)
-                    let nombreDelHeroe = datosHeroes["results"]["name"]
-                    document.getElementById("id").src = nombreDelHeroe
-                
+    let nombreHeroe = document.getElementById("inputNombreHeroe").value;
+    let listaDeheroes = new XMLHttpRequest;
+    listaDeheroes.open("GET", `https://superheroapi.com/api.php/${token}/search/${nombreHeroe}`);
+    listaDeheroes.onreadystatechange = function(){
+        if(listaDeheroes.readyState === 4 && listaDeheroes.status === 200){
+            try{
+                datosHeroe = JSON.parse(listaDeheroes.responseText);
+                for(let i = 0; i < datosHeroe["results"].length; i++){
+                    let name = datosHeroe["results"][i]["name"]
+                    let image = datosHeroe["results"][i]["image"]["url"]
+                    let tarjetasPersonajes = document.createRange().createContextualFragment(`
+                       <div class="tarjetaSola">
+                        <div>
+                        <img src="${image}" alt="${name}">
+                        </div>
+                        <h2>${name}</h2>
+                       </div>
+                        
+                        `)
+                    document.getElementById("tarjetaPersonaje").append(tarjetasPersonajes)
+                    
                 }
+
+            }
+
+            catch(err){
+
+                console.log(err.message);
+            }
         }
-            
-        }
-    llamadaHeroe.se
-    })
+    }
+       
     
+    listaDeheroes.send();
 }
 
